37a93459c97f71da739db4a4d23c1c6b9e01d3f2
VedangSardessai <vms112000@gmail.com>
2023-03-05 23:56:36 +0530

    code: actions with dispatch added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import styled from "styled-components";
 import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { contactState } from "./reducers/contactSlice";
+
 const Title = styled.h1`
   font-size: 1.5em;
   text-align: center;
@@ -62,8 +67,7 @@ const AddContact = styled.button`
 
 const DeleteContact = styled.button`
   color: white;
-  margin: 25px;
-  width: 250px;
+  margin: 5px 0 5px 0;
   height: 50px;
   font-size: 20px;
   background-color: red;
@@ -88,22 +92,84 @@ function App() {
   const [phone, setPhone] = useState("");
   const [search, setSearch] = useState("");
 
+  const contacts = useSelector((state) => state);
+
+  const dispatch = useDispatch();
+  const contactArr = contacts.contactReducer;
+  const [dataArray, setDataArray] = useState(contactArr);
+
   const addContact = (e) => {
     e.preventDefault();
-    if (name == "") setNameError(true);
-    if (phone == "") setPhoneError(true);
-    else if (name != "" && phone != "") {
-      setNameError(false);
-      setPhoneError(false);
+    if (!name) setNameError(true);
+    if (!phone) setPhoneError(true);
 
-      console.log(name, "name");
-      console.log(phone, "phone");
+    console.log(contacts);
+    if (phone) setPhoneError(false);
+
+    if (name) setNameError(false);
+
+    const checkName = contactArr.find(
+      (contact) => contact.name === name && name
+    );
+
+    const checkPhone = contactArr.find(
+      (contact) => contact.number === phone && phone
+    );
+
+    if (checkName || checkPhone)
+      return toast.error("Contact Already Exists", {
+        position: "top-right",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
+
+    if (phone && name) {
+      const contactData = {
+        id:
+          contactArr.length > 0 ? contactArr[contactArr.length - 1].id + 1 : 0,
+        name: name,
+        number: phone,
+      };
+
+      dispatch({
+        type: "ADD_CONTACT",
+        payload: contactData,
+      });
+
+      toast.success("Contact Saved");
+      // console.log(contactData);
     }
+
+    console.log(contactState);
   };
+
+  const deleteContact = (id) => {
+    dispatch({
+      type: "DELETE_CONTACT",
+      payload: id,
+    });
+  };
+
+  useEffect(() => {
+    setDataArray(contacts.contactReducer);
+    console.log(contacts.contactReducer);
+  }, [contacts.contactReducer]);
 
   return (
     <div className="App">
       <header className="App-header">
+        {/* Same as */}
+        <ToastContainer
+          style={{
+            color: "red",
+            fontSize: "18px",
+          }}
+        />
         <Title>Phone Directory</Title>
         <SearchBar
           name="searchInput"
@@ -114,7 +180,7 @@ function App() {
 
         <br />
         <br />
-
+        <div>{contacts.contactReducer.name}</div>
         <Title>Add New Contact Or Delete Contact</Title>
         <GridCont>
           <Name
@@ -136,9 +202,37 @@ function App() {
 
         <GridCont>
           <AddContact onClick={addContact}>Add Contact</AddContact>
-          <DeleteContact>Delete Contact</DeleteContact>
         </GridCont>
       </header>
+      {contactArr.length > 0 ? (
+        <table id="customers">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Phone</th>
+              <th>Delete</th>
+            </tr>
+          </thead>
+          {dataArray.map((friend, index) => (
+            <>
+              <tr key={index}>
+                <td>{friend.id}</td>
+                <td>{friend.name}</td>
+                <td>{friend.number}</td>
+                <td>
+                  {" "}
+                  <DeleteContact onClick={() => deleteContact(friend.id)}>
+                    Delete Contact
+                  </DeleteContact>
+                </td>
+              </tr>
+            </>
+          ))}
+        </table>
+      ) : (
+        <Title>Contact List Is Empty</Title>
+      )}
     </div>
   );
 }
